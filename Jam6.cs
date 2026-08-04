0f31d3519c9d7fb64be54c0c3b9c43f8224e0e55
CrystalENVT <email@crystal.envt.gay>
2026-08-03 22:06:56 -0500

    Add closer test planet, & simplify namespace in Jam6

--- Jam6.cs
+++ Jam6.cs
@@ -3,42 +3,49 @@ using OWML.Common;
 using OWML.ModHelper;
 using System.Reflection;
 
-namespace Jam6
+namespace Jam6;
+
+public class Jam6 : ModBehaviour
 {
-    public class Jam6 : ModBehaviour
+    public static Jam6 Instance;
+    public INewHorizons NewHorizons;
+
+    public bool AllowSpawnOverride { get; private set; }
+
+    public void Awake()
     {
-        public static Jam6 Instance;
-        public INewHorizons NewHorizons;
-
-        public void Awake()
-        {
-            Instance = this;
-            // You won't be able to access OWML's mod helper in Awake.
-            // So you probably don't want to do anything here.
-            // Use Start() instead.
-        }
-
-        public void Start()
-        {
-            // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"My mod {nameof(Jam6)} is loaded!", MessageType.Success);
-
-            // Get the New Horizons API and load configs
-            NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
-            NewHorizons.LoadConfigs(this);
-
-            new Harmony("T3rtu.Jam6").PatchAll(Assembly.GetExecutingAssembly());
-
-            // Example of accessing game code.
-            OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
-            LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
-        }
-
-        public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
-        {
-            if (newScene != OWScene.SolarSystem) return;
-            ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
-        }
+        Instance = this;
+        // You won't be able to access OWML's mod helper in Awake.
+        // So you probably don't want to do anything here.
+        // Use Start() instead.
     }
 
+    public void Start()
+    {
+        // Starting here, you'll have access to OWML's mod helper.
+        ModHelper.Console.WriteLine($"My mod {nameof(Jam6)} is loaded!", MessageType.Success);
+
+        // Get the New Horizons API and load configs
+        NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+        NewHorizons.LoadConfigs(this);
+
+        new Harmony("T3rtu.Jam6").PatchAll(Assembly.GetExecutingAssembly());
+
+        // Example of accessing game code.
+        OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
+        LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
+    }
+
+    public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
+    {
+        if (newScene != OWScene.SolarSystem) return;
+        ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+    }
+
+    public override void Configure(IModConfig config)
+{
+    base.Configure(config);
+
+    AllowSpawnOverride = config.GetSettingsValue<bool>("allowSpawnOverride");
+}
 }
