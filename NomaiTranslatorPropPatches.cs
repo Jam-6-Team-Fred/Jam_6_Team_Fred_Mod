59a96b6eee439da61a258a639e71b0fa1003c454
CrystalENVT <email@crystal.envt.gay>
2026-08-11 22:58:42 -0500

    Add last of Nomai Text & add Dynamic Text patch
    Need to format some larger text elements still in world
    
    Need to connect the translated text with the Schedule Event system after these features are both merged

---
+++ NomaiTranslatorPropPatches.cs
@@ -0,0 +1,28 @@
+// Code originally designed by Hawkbar/Hawkbat
+//   Original Source:
+//   https://github.com/Hawkbat/OuterWildsModJam5/blob/main/Terrarium/Patches/NomaiTranslatorPropPatches.cs
+
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jam6
+{
+    [HarmonyPatch(typeof(NomaiTranslatorProp))]
+    public static class NomaiTranslatorPropPatches
+    {
+        [HarmonyPostfix, HarmonyPatch(nameof(NomaiTranslatorProp.SetNomaiText), typeof(NomaiText), typeof(int))]
+        public static void SetNomaiText(NomaiTranslatorProp __instance)
+        {
+            var text = __instance._textNodeToDisplay;
+            if (text.Contains("$TEAMFRED_EXPO_"))
+            {
+                text = text.Replace("$TEAMFRED_EXPO_TIME", "foobar");
+                __instance._textNodeToDisplay = text;
+            }
+        }
+    }
+}
