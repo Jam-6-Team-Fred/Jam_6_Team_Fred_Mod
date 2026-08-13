177167efb2c5ecf1131492140011baee9e494306
Hamstry <maxikon777@gmail.com>
2026-08-13 23:38:42 +0300

    new scheduling scripts

--- SchedulingExpoHall.cs
+++ SchedulingExpoHall.cs
@@ -7,17 +7,20 @@ using UnityEngine.InputSystem;
 
 namespace Jam6
 {
-    public class SchedulingTest : MonoBehaviour
+    public class SchedulingExpoHall : MonoBehaviour
     {
         [NonSerialized]
         public ModBehaviour mod;
         [NonSerialized]
         public MeshRenderer meshRenderer;
+        [NonSerialized]
+        public MeshCollider meshCollider;
 
         public void Awake()
         {
             mod = Jam6.Instance;
             meshRenderer = GetComponent<MeshRenderer>();
+            meshCollider = GetComponent<MeshCollider>();
             SchedulingSocket.ActivateScheduledEvent += Disappear;
             SchedulingSocket.DeactivateScheduledEvent += Appear;
         }
@@ -31,20 +34,20 @@ namespace Jam6
         public void Disappear(SchedulingItem item)
         {
             mod.ModHelper.Console.WriteLine("Got Activate Event", OWML.Common.MessageType.Success);
-            if (item.itemID == "test")
+            if (item.itemID == "ExpoHall")
             {
                 meshRenderer.enabled = false;
-                mod.ModHelper.Console.WriteLine($"Its test, mesh: {meshRenderer.enabled}", OWML.Common.MessageType.Success);
+                mod.ModHelper.Console.WriteLine($"Its ExpoHall, mesh: {meshRenderer.enabled}", OWML.Common.MessageType.Success);
             }
         }
 
         public void Appear(SchedulingItem item)
         {
             mod.ModHelper.Console.WriteLine("Got Deactivate Event", OWML.Common.MessageType.Success);
-            if (item.itemID == "test")
+            if (item.itemID == "ExpoHall")
             {
                 meshRenderer.enabled = true;
-                mod.ModHelper.Console.WriteLine($"Its test, mesh: {meshRenderer.enabled}", OWML.Common.MessageType.Success);
+                mod.ModHelper.Console.WriteLine($"Its ExpoHall, mesh: {meshRenderer.enabled}", OWML.Common.MessageType.Success);
             }
         }
     }
