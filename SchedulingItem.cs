9c018dd00cb51e2e1dcb2b8b415c222667f9d0c1
Hamstry <maxikon777@gmail.com>
2026-08-15 21:01:46 +0300

    assetbundles

--- SchedulingItem.cs
+++ SchedulingItem.cs
@@ -10,14 +10,19 @@ namespace Jam6
         [SerializeField]
         public string itemID;
 
+<<<<<<< Updated upstream
+=======
+        [NonSerialized]
+        public GameObject hologram;
+>>>>>>> Stashed changes
         [NonSerialized]
         public ModBehaviour mod;
 
         public void OnValidate()
         {
             _type = Jam6.SchedulingItemType;
-            DisplayName = "SchedulingItemDisplayName";
             Droppable = true;
+            hologram = transform.GetChild(1).gameObject;
         }
 
         public override void Awake()
@@ -26,6 +31,27 @@ namespace Jam6
             base.Awake();
             mod = Jam6.Instance;
             mod.ModHelper.Console.WriteLine("A scheduling item is created", OWML.Common.MessageType.Success);
+        }
+
+        public override void PickUpItem(Transform holdTranform)
+        {
+            base.PickUpItem(holdTranform);
+            mod.ModHelper.Console.WriteLine("I got picked up", OWML.Common.MessageType.Success);
+            hologram.SetActive(false);
+        }
+
+        public override void DropItem(Vector3 position, Vector3 normal, Transform parent, Sector sector, IItemDropTarget customDropTarget)
+        {
+            base.DropItem(position, normal, parent, sector, customDropTarget);
+            mod.ModHelper.Console.WriteLine("I got picked down", OWML.Common.MessageType.Success);
+            hologram.SetActive(true);
+        }
+
+        public override void SocketItem(Transform socketTransform, Sector sector)
+        {
+            base.SocketItem(socketTransform, sector);
+            mod.ModHelper.Console.WriteLine("I got socketed", OWML.Common.MessageType.Success);
+            hologram.SetActive(true);
         }
     }
 }
---
+++ SchedulingRain.cs
@@ -0,0 +1,46 @@
+﻿using OWML.ModHelper;
+using OWML.Utils;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Jam6
+{
+    public class SchedulingRain : MonoBehaviour
+    {
+        [NonSerialized]
+        public ModBehaviour mod;
+
+        public void Awake()
+        {
+            mod = Jam6.Instance;
+            SchedulingSocket.ActivateScheduledEvent += Activate;
+            SchedulingSocket.DeactivateScheduledEvent += Deactivate;
+        }
+
+        public void OnDestroy()
+        {
+            SchedulingSocket.ActivateScheduledEvent -= Activate;
+            SchedulingSocket.DeactivateScheduledEvent -= Deactivate;
+        }
+
+        public void Activate(SchedulingItem item)
+        {
+            mod.ModHelper.Console.WriteLine("Got Activate Event", OWML.Common.MessageType.Success);
+            if (item.itemID == "Rain")
+            {
+
+            }
+        }
+
+        public void Deactivate(SchedulingItem item)
+        {
+            mod.ModHelper.Console.WriteLine("Got Deactivate Event", OWML.Common.MessageType.Success);
+            if (item.itemID == "Rain")
+            {
+
+            }
+        }
+    }
+}
