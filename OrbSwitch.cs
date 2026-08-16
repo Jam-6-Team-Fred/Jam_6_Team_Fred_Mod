c0eb415dd73422404cf585cda5d82868a3c3a8ee
Hamstry <157906105+Hamstryk@users.noreply.github.com>
2026-08-16 16:03:45 +0300

    Fixed lights (#32)
    * fixed lights
    
    * Update SchedulingItem.cs

--- OrbSwitch.cs
+++ OrbSwitch.cs
@@ -52,11 +52,8 @@ namespace Jam6
             {
                 _powerOn = true;
 
-                //gravity1.SetActive(false);
-                //gravity2.SetActive(false);
                 gravity1.GetComponent<DirectionalForceVolume>().SetFieldMagnitude(0);
                 gravity2.GetComponent<DirectionalForceVolume>().SetFieldMagnitude(0);
-                Jam6.Instance.ModHelper.Console.WriteLine($"Gravities: 1-{gravity1.active}, 2-{gravity2.active}", OWML.Common.MessageType.Success);
 
                 if (_audioSource != null)
                 {
@@ -71,11 +68,8 @@ namespace Jam6
             {
                 _powerOn = false;
 
-                //gravity1.SetActive(true);
-                //gravity2.SetActive(true);
                 gravity1.GetComponent<DirectionalForceVolume>().SetFieldMagnitude(12);
                 gravity2.GetComponent<DirectionalForceVolume>().SetFieldMagnitude(12);
-                Jam6.Instance.ModHelper.Console.WriteLine($"Gravities: 1-{gravity1.active}, 2-{gravity2.active}", OWML.Common.MessageType.Success);
 
                 if (_audioSource != null)
                 {
@@ -101,8 +95,8 @@ namespace Jam6
             if (planetName == "Disc")
             {
                 Jam6.Instance.ModHelper.Console.WriteLine("Found Disc", OWML.Common.MessageType.Success);
-                gravity1 = SearchUtilities.Find("Disc/Past/GravityVolume");
-                gravity2 = SearchUtilities.Find("Disc/Future/GravityVolume");
+                gravity1 = SearchUtilities.Find("Disc_Body/Disc/Past/GravityVolume");
+                gravity2 = SearchUtilities.Find("Disc_Body/Disc/Future/GravityVolume");
             }
         }
     }
